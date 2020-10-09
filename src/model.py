fb27815f6603226027dd8659ec6a5a323b4f5ea4
chris pryer <christophpryer@gmail.com>
2020-10-08 21:29:43 -0400

    refactored model.py

--- src/model.py
+++ src/model.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple
 from collections import namedtuple
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
-import plotly.graph_objects as go
 
 
 def get_dropped_nodes(
@@ -213,68 +212,3 @@ def solve(
             solution.append(route)
 
         return solution
-
-
-def visualize_solution(vehicles: "Vehicles") -> None:
-    # base
-    lats = []
-    lons = []
-    text = []
-
-    # lines
-    lat_paths = []
-    lon_paths = []
-    for i, r in enumerate(vehicles):
-        for j, v in enumerate(r):
-            lats.append(v.lat)
-            lons.append(v.lon)
-            text.append(f"demand: {v.demand}")
-
-            if j < len(r) - 1:
-                lat_paths.append([v.lat, r[j + 1].lat])
-                lon_paths.append([v.lon, r[j + 1].lon])
-
-    fig = go.Figure()
-
-    for i in range(len(lat_paths)):
-        fig.add_trace(
-            go.Scattergeo(
-                locationmode="USA-states",
-                lat=[lat_paths[i][0], lat_paths[i][1]],
-                lon=[lon_paths[i][0], lon_paths[i][1]],
-                mode="lines",
-                line=dict(width=1, color="red"),
-                # opacity = float(df_flight_paths['cnt'][i]) / float(df_flight_paths['cnt'].max()),
-            )
-        )
-
-    fig.add_trace(
-        go.Scattergeo(
-            locationmode="USA-states",
-            lat=lats,
-            lon=lons,
-            hovertext=text,
-            hoverinfo="text",
-            mode="markers",
-            marker=dict(
-                size=5,
-                color="rgb(255, 0, 0)",
-                line=dict(width=3, color="rgba(68, 68, 68, 0)"),
-            ),
-        )
-    )
-
-    fig.update_layout(
-        title_text="",
-        showlegend=False,
-        template="plotly_dark",
-        geo=dict(
-            scope="north america",
-            projection_type="azimuthal equal area",
-            showland=True,
-            landcolor="rgb(243, 243, 243)",
-            countrycolor="rgb(204, 204, 204)",
-        ),
-    )
-
-    fig.show()
